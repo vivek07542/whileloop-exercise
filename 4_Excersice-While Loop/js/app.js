11a54740491cdec41dc74d43105ef6505942c267
unknown <vivekbindal1209@gmail.com>
2020-08-30 07:36:19 +0530

    While Loop Condition Exercise

---
+++ 4_Excersice-While Loop/js/app.js
@@ -0,0 +1,29 @@
+//1) Question
+var num = -10;
+ while(num<=19) {
+  console.log(num);
+   num++;
+ }
+
+//2) Question
+ var num = 10;
+  while(num <= 40 ) {
+    if(num % 2 ===0)
+    console.log(num);
+    num++;
+  } 
+ //3) Even Question
+ var num = 300;
+  while( num <=333) {
+    if(!(num % 2 ===0))
+    console.log(num);
+    num++;
+  }
+  //4) Divide Question
+  var num = 5;
+   while( num <=50) {    
+     if (num % 5 ===0)
+     if (num % 3 ===0)
+      console.log(num);
+      num++;
+   }
